ea1da384e60fa6ceec406fb07cab9b1753f8f391
Miguel-Aguilar19 <miguel.aguilar@ucb.edu.bo>
2024-10-04 10:48:10 -0400

    clase 01/10/24

--- CLASES/03 LISTA ENTEROS/js/ListaEntero.js
+++ CLASES/03 LISTA ENTEROS/js/ListaEntero.js
@@ -25,6 +25,69 @@ class ListaEnteros {
 
 // Instancia de la clase ListaEnteros
 const listaEnteros = new ListaEnteros();
+listaEnteros.agregar(21);
+listaEnteros.agregar(12);
+listaEnteros.agregar(15);
+listaEnteros.agregar(30);
+console.log(listaEnteros.lista); //imprime: [21,12,15,30]
+
+var a = listaEnteros.lista[0];
+console.log(a);
+
+var b = listaEnteros.lista[1];
+console.log(b);
+
+var c = listaEnteros.lista[2];
+console.log(c);
+
+var d = listaEnteros.lista[3];
+console.log(d);
+
+//RETORNAR EL ELEMENTO MENOR DE LA LISTA, DADO QUE LA LISTA SOLO ALMACENA VALORES POSTIVOS
+/*
+
+var datoMenor = -1;
+
+//-----------------------------------------------------
+var a = listaEnteros.lista[0];
+if(a > datoMenor){
+    datoMenor = a;
+}
+console.log("Dato Menor: " + datoMenor);
+//----------------------------------------------------
+var b = listaEnteros.lista[1];
+if(a > b){
+    datoMenor = b;
+}
+console.log("Dato Menor:" + datoMenor);
+//----------------------------------------------------
+var c = listaEnteros.lista[2];
+if(b > c){
+    datoMenor = c;
+}
+console.log("dato Menor:" + datoMenor);
+//---------------------------------------------------
+var d = listaEnteros.lista[3];
+if(c > d){
+    datoMenor = d;
+}
+console.log("dato Menor;" + datoMenor);
+//--------------------------------------------------
+*/
+
+var datoMenor = -1; 
+
+for (var i = 0; i < listaEnteros.lista.length; i++) {
+    var valorActual = listaEnteros.lista[i];
+
+    if (i === 0 || valorActual < datoMenor) {
+        datoMenor = valorActual;
+    }
+
+    console.log("dato menor:" + datoMenor);
+
+}
+
 
 // Referencias a los elementos HTML
 const input = document.getElementById('numeroInput');
